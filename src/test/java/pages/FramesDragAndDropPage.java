e23a4bca8a3dbb9db785aa35e5122485b6f6d7fa
Igor <igorvolynets1985@gmail.com>
2022-03-03 19:19:06 +0300

    Work

--- src/test/java/pages/FramesDragAndDropPage.java
+++ src/test/java/pages/FramesDragAndDropPage.java
@@ -11,16 +11,34 @@ public class FramesDragAndDropPage extends BasePage{
     private By DRAGMETOMYTARGETFIELD = By.xpath("//div[@id='draggable']");
     private By DROPHEREFIELD = By.xpath("//div[@id='droppable']");
     private By FRAMEAREA = By.xpath("//iframe[@class='demo-frame']");
+    private By TEXT = By.xpath("//div[@id='droppable']/p");
+
+    public By getTEXT() {
+        return TEXT;
+    }
 
     public FramesDragAndDropPage(WebDriver driver) {
         super(driver);
     }
 
+    Actions actions;
 
-    public void method(){
+    public String text2;
+
+    public String dragChecking() throws InterruptedException {
         driver.get(WebUrls.FRAMESDRAGANDDROP_URL);
+        Thread.sleep(1000);
         WebElement frame = driver.findElement(FRAMEAREA);
         driver.switchTo().frame(frame);
+        WebElement target1 = driver.findElement(DRAGMETOMYTARGETFIELD);
+        WebElement target2 = driver.findElement(DROPHEREFIELD);
+        actions = new Actions(driver);
+        actions.dragAndDrop(target1, target2).perform();
+
+        Thread.sleep(1000);
+        return driver.findElement(TEXT).getText();
+
+
 
     }
 }
--- src/test/java/tests/FramesDragAndDrop.java
+++ src/test/java/tests/FramesDragAndDrop.java
@@ -1,8 +1,17 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
 import pages.FramesDragAndDropPage;
 
-public class FramesDragAndDrop {
+public class FramesDragAndDrop extends BaseTest{
 
     FramesDragAndDropPage framesDragAndDropPage;
+
+    @Test
+    public void checkText() throws InterruptedException {
+        framesDragAndDropPage = new FramesDragAndDropPage(driver);
+        framesDragAndDropPage.dragChecking();
+        Assert.assertEquals(framesDragAndDropPage.getTEXT(), "Dropped!");
+    }
 }
